2f315c58f4efd009de8839576fc034a0141c904c
Aidan Haile <tacticalfluke304@gmail.com>
2022-07-16 10:53:56 +0800

    Add stat block + items

---
+++ StatBlock.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name StatBlock
+
+export (int) var max_health = 0
+export (int) var max_speed = 0
+export (float) var damage_multiplier = 0
+
+export (float) var acceleration = 0
