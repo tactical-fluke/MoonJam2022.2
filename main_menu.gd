3083ec9403cd0de08fe3f14addd4026734af5bb5
Aidan Haile <tacticalfluke304@gmail.com>
2022-07-17 14:43:53 +0800

    Add base main menu

---
+++ main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+export (PackedScene) var start_scene
+export (PackedScene) var settings_menu
+
+func _ready():
+	$PlayButton.self_modulate.a = 0
+	$SettingsButton.self_modulate.a = 0
+
+func _on_PlayButton_pressed():
+	print("play button pressed")
+
+func _on_PlayButton_mouse_entered():
+	$PlayButton.self_modulate.a = 0.3
+
+func _on_PlayButton_mouse_exited():
+	$PlayButton.self_modulate.a = 0
+
+func _on_SettingsButton_pressed():
+	print("settings button pressed")
+
+func _on_SettingsButton_mouse_entered():
+	$SettingsButton.self_modulate.a = 0.3
+
+func _on_SettingsButton_mouse_exited():
+	$SettingsButton.self_modulate.a = 0
