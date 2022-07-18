af9f596ba0d8283cd1c8ede4440d81839db9af25
Aidan Haile <tacticalfluke304@gmail.com>
2022-07-18 14:35:10 +0800

    Add some items

--- items/item.gd
+++ items/item.gd
@@ -13,6 +13,8 @@ var xb_glyph = preload("res://Assets/Textures/xbox.png")
 
 var in_range = false
 
+var stay_hidden = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$PanelContainer/Label.text = hover_description
@@ -32,11 +34,14 @@ func interacted():
 	player_stat_block.acceleration += stat_block.acceleration
 	player_stat_block.damage_modifier += stat_block.damage_modifier
 	player_stat_block.fire_cooldown_modifer += stat_block.fire_cooldown_modifer
-	queue_free()
+	show_extended_description()
+	$Sprite.hide()
+	$PanelContainer.hide()
+	$PickupIcon.hide()
 	
 
 func _on_Area2D2_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") && !stay_hidden:
 		$PanelContainer.show()
 		$PickupIcon.show()
 		in_range = true
@@ -55,3 +60,13 @@ func _set_glyph(data):
 		$PickupIcon.set_texture(xb_glyph)
 	if data == 'ps':
 		$PickupIcon.set_texture(ps_glyph)
+		
+func show_extended_description():
+	$Control/CanvasLayer/Dialogue.lines[0] = extended_description
+	$Control/CanvasLayer/Dialogue.start_dialogue()
+	$Control/CanvasLayer/Dialogue.connect("line_finished", self, "delete_extended_description")
+	stay_hidden = true
+
+
+func delete_extended_description():
+	queue_free()
