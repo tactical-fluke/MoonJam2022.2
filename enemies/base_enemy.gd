bbca044abc64cd0a38e68763003ee5c025ee8b90
Prodirus <prodirus@gmail.com>
2022-07-18 23:38:48 +0100

    ppPoof

--- enemies/base_enemy.gd
+++ enemies/base_enemy.gd
@@ -52,11 +52,12 @@ func _process(delta):
 		_:
 			pass
 	
+	
 	handle_animation()
 
 # Area trigger to put the enemy into fight mode
 func _on_area_trigger_entered_by_player():
-	if state != EnemyState.DEAD:
+	if state != EnemyState.DEAD && state != EnemyState.POOF:
 		state = EnemyState.ATTACK
 
 # Per frame attack process
@@ -97,8 +98,14 @@ func _on_FireTimer_timeout():
 	$FireTimer.stop()
 	
 func handle_projectile(projectile, damage):
-	projectile.queue_free()
-	health.take_damage(damage)
+
+	match state:
+		EnemyState.POOF:
+			$CollisionShape2D.disabled = true
+			pass
+		_:
+			projectile.queue_free()
+			health.take_damage(damage)
 	
 func get_side_of_player() -> String:
 	var left = get_direction_to_player().x < 0
@@ -107,22 +114,22 @@ func get_side_of_player() -> String:
 func handle_death():
 	state = EnemyState.DEAD
 	dead_direction = get_side_of_player()
-	death_timer.connect("timeout",self,"ppPoof")
-	death_timer.wait_time = 3
-	death_timer.one_shot = true
-	add_child(death_timer)
-	death_timer.start()		
 	
 func handle_animation():
 	var anim_suffix = get_side_of_player()
 	match state:
 		EnemyState.DEAD:
 			$Sprite.play("death_%s" % dead_direction)
+			yield(get_tree().create_timer(1.0), "timeout")
+			state = EnemyState.POOF
+		EnemyState.POOF:
+			$Sprite.play("poof")
+			print($Sprite.animation)
 		_: #TODO
 			$Sprite.play("idle_%s" % anim_suffix)
 
-func ppPoof():
-	state = EnemyState.POOF
-	$Sprite.play("poof")
-	hide()
-	queue_free()
+func _on_Sprite_animation_finished():
+	print("pppoof")
+	if ($Sprite.animation == "poof"):
+		hide()
+		queue_free()
